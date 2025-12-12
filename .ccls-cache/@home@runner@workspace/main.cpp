243f8b0b7dfff2c3dd8caef0c342578459534701
tobam <tobamohaqqeq84@gmail.com>
2025-12-12 00:24:55 +0000

    3rd

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -10,8 +10,8 @@ string drink;
 CoffeeCustomerNode* next;
 };
 
-void enqueue(CoffeeCustomerNode*& head, CoffeeCustomerNode*& tail, const string& name, const string& drink){
-    coffeeCustomerNode* newNode = new CoffeeCustomerNode{name, drink, nullptr};
+void enqueueCoffee(CoffeeCustomerNode*& head, CoffeeCustomerNode*& tail, const string& name, const string& drink){
+    coffeeCustomer* newNode = new CoffeeCustomerNode{name, drink, nullptr};
 
      if(tail == nullptr){
          head = tail = newNode;
@@ -21,4 +21,38 @@ void enqueue(CoffeeCustomerNode*& head, CoffeeCustomerNode*& tail, const string&
          tail = newNode;
     }
 }
-bool dequque(CoffeeCustomerNode*& head, CoffeeCustomerNode*& tail, string& name, string& drink)
+bool dequque(CoffeeCustomerNode*& head, CoffeeCustomerNode*& tail, string& servedName, string& servedDrink){
+     if(head == nullptr){
+         return false;
+     }
+    coffeeCustomerNode* temp = head;
+    servedName = head->name;
+    servedDrink = head->drink;
+
+    head = head->next;
+     if(head == nullptr){
+         tail = nullptr;
+     }
+     delete temp;
+    return true; 
+}
+
+bool isCoffeeQueueEmpty(CoffeeCustomerNode* head){
+     return head == nullptr;
+}
+void printCoffeeQueue(CoffeeCustomerNode* head){
+     cout << "[Coffee Queue]: ";
+
+    if (head == nullptr){
+         cout << "Empty" << endl;
+    } else{
+         CoffeeCustomerNode* current = head;
+         while(current != nullptr){
+             cout << "(" <<current->name << current->drink << ") ";
+             if(current->next != nullptr)
+                 cout << "-> ";
+             current = current->next;
+         }
+    }
+    cout << endl;
+}

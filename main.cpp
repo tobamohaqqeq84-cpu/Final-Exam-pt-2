bc0b811287ebe729f30ca027a4ef07cd25ff0c7c
tobam <tobamohaqqeq84@gmail.com>
2025-12-12 00:14:51 +0000

    2nd

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 using namespace std;
 
-st
+struct CoffeeCustomerNode{
+
+string name;
+string drink;
+CoffeeCustomerNode* next;
+};
+
+void enqueue(CoffeeCustomerNode*& head, CoffeeCustomerNode*& tail, const string& name, const string& drink){
+    coffeeCustomerNode* newNode = new CoffeeCustomerNode{name, drink, nullptr};
+
+     if(tail == nullptr){
+         head = tail = newNode;
+     }
+    else {
+         tail->next = newNode;
+         tail = newNode;
+    }
+}
+bool dequque(CoffeeCustomerNode*& head, CoffeeCustomerNode*& tail, string& name, string& drink)
